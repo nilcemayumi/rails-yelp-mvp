88c9976f1f5f989f76b47d24cb6627237ed32c78
Nilce Moscone <nilcemayumi@gmail.com>
2023-01-19 16:07:37 -0300

    restaurant model, controller, views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating database'
+
+100.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
